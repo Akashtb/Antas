de927a924043d2f4af8482030315b0333f5e4236
akashtb <akashtb21@gamil.com>
2023-03-16 00:15:13 +0530

    message

--- antas-client/src/components/content/Content.jsx
+++ antas-client/src/components/content/Content.jsx
@@ -1,7 +1,206 @@
 import React from 'react'
-
+import './content.css'
 export default function Content() {
   return (
-    <div>Content</div>
+    <div className="outerContainer">
+
+      <div className='innerContainer'>
+        <div className='topContainer'>
+          <div className='topContainerBody'>
+            <span className='content'>
+              <h1 className='heading'>Delivering Excellence</h1>
+              <span className='subHeading'>
+                We are a team of subject-matter experts who are in the loop about the latest innovations and look out for talents in a wide range of industries.
+              </span>
+            </span>
+          </div>
+          <img className='imageHeader' src="https://itbusinesstoday.com/wp-content/uploads/2020/11/Revamping-the-Legacy-IT-infrastructure-for-a-Better-Future.jpg" alt="" />
+        </div>
+        <div>
+        </div>
+      </div>
+
+      <div className='secondContainer'>
+        <div className='imageContainer'>
+          <img src="https://static.wixstatic.com/media/aaaf4c7f4a4e46d5931ecc2ff934cf49.jpg/v1/fill/w_916,h_672,fp_0.50_0.50,q_85,usm_0.66_1.00_0.01,enc_auto/aaaf4c7f4a4e46d5931ecc2ff934cf49.jpg" alt="" className="imageContainer" />
+        </div>
+        <div className='contentContainer'>
+          <span><h2>About Antas</h2></span>
+          <br />
+          <span className='subContent'>Antas is a global IT services provider and contingent staffing firm headquartered in Singapore with offices in UAE, UK, and India.</span>
+          <br />
+          <span className='subContent'>Antas is the definitive point between business and technology. At Antas, we adhere to the highest industry standards available to ensure you are given the business intelligence you need to stay competitive in the modern marketplace. Our immense client support has provided us an edge over our peers, enabling us to offer a comprehensive solution in all the means.  </span>
+
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div className='thirdContainer'>
+        <div className="thirdMainHeader">
+          <span className='thirdMainHeaderSub'>Why Choose Us?</span>
+        </div>
+        <br />
+        <div className="thirdContent">
+          <span className='thirdContentSub'>
+            Antas has the knowledge, experience, and commitment to serve all our clientele. Whether you are looking for a job or recruiting new employees for your company, our expert team members will offer you their full attention and guidance so that you can make the most out of our services. Contact us to learn more about the features we offer our clients.
+          </span>
+        </div>
+      </div>
+
+      <div className='cardgroup'>
+        <div className='card-container'>
+          <div className="image-container">
+            <img src="https://static.wixstatic.com/media/845bf6_53698bebe212428aab7fd749b02ae0b9~mv2.jpg/v1/fill/w_421,h_357,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Approach.jpg" alt="" />
+          </div>
+          <div className="card-content">
+            <div className="card-body">
+              <ul>
+                <li>Focused search for every position assigned to us</li>
+                <li>Rich and updated internal database</li>
+                <li>Best search tools and research capability</li>
+                <li>Diversity, personalised and unbiased searches.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className='card-container'>
+          <div className="image-container">
+            <img src="https://static.wixstatic.com/media/845bf6_6e7df02c26ea4381817224dc70756bf2~mv2.jpg/v1/fill/w_407,h_344,al_c,lg_1,q_80,enc_auto/Client%20Management.jpg" alt="" />
+          </div>
+          <div className="card-content">
+            <div className="card-body">
+              <ul>
+                <li>Every client is our strategic partner</li>
+                <li>Adhere to client process with confidentiality</li>
+                <li>Client centric management of candidates</li>
+                <li>High priority searches with timelines</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className='card-container'>
+          <div className="image-container3">
+            <img src="https://static.wixstatic.com/media/845bf6_4d67a9b17db6459998f1726444afa3b6~mv2.jpg/v1/fill/w_407,h_344,al_c,lg_1,q_80,enc_auto/Dynamic%20Searches.jpg" alt="" />
+          </div>
+          <div className="card-content">
+            <div className="card-body">
+              <ul>
+                <li>Customised talent search for every assignment</li>
+                <li>Through assessment of job requirements and scope</li>
+                <li>Ensure clear understanding of client’s culture and corporate policy</li>
+                <li>Aggressive search that meets the SLA</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div >
+
+
+      <div className="fifthContainer">
+        <div className="fifthHeading">
+          <span className="fifthMainHeading">Carrers</span>
+        </div>
+        <div className="fifthContent">
+          <span className='fifthMainContent'>Attracting and developing the best talent is always our priority Job seekers -Connect with us for delivering excellence.</span>
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="SimpleContainer">
+        <div className='simpleHeading'>
+          <span className='simpleMainHeading'>Our locations</span>
+        </div>
+      </div>
+
+      <br />
+      <br />
+
+
+      <div className='countryGroup'>
+        <div className='countryContainer'>
+          <div className='countryImageContainer'>
+            <img src="https://cdn-icons-png.flaticon.com/128/4926/4926536.png" alt="" />
+          </div>
+          <div className='countryContent'>
+            <div className='countryName'>
+              <h3>SINGAPORE</h3>
+            </div>
+            <div className='countryDetails'>
+              <p>Odeon Towers #02-01,
+                331 North Bridge Road, Singapore 188720
+                Contact: +65 61009786</p>
+            </div>
+          </div >
+        </div>
+
+
+        <div className='countryContainer'>
+          <div className='countryImageContainer'>
+            <img src="https://cdn-icons-png.flaticon.com/128/4926/4926536.png" alt="" />
+          </div>
+          <div className='countryContent'>
+            <div className='countryName'>
+              <h3>DUBAI</h3>
+            </div>
+            <div className='countryDetails'>
+              <p>ANTAS TECHNOLOGIES,
+
+                Office 309, 3rd Floor, Block-C
+
+                Hamdan Award Complex, Jumeirah 1, Dubai, UAE. Contact +971 501571287</p>
+            </div>
+          </div >
+        </div>
+
+        <div className='countryContainer'>
+          <div className='countryImageContainer'>
+            <img src="https://cdn-icons-png.flaticon.com/128/4926/4926536.png" alt="" />
+          </div>
+          <div className='countryContent'>
+            <div className='countryName'>
+              <h3>UK</h3>
+            </div>
+            <div className='countryDetails'>
+              <p>Imperial Offices
+
+                E6 2JG Suit #10
+                2A Heigham Road
+                East Ham, London
+
+                Contact +44 7403467467</p>
+            </div>
+          </div >
+        </div>
+
+        <div className='countryContainer'>
+          <div className='countryImageContainer'>
+            <img src="https://cdn-icons-png.flaticon.com/128/4926/4926536.png" alt="" />
+          </div>
+          <div className='countryContent'>
+            <div className='countryName'>
+              <h3>INDIA</h3>
+            </div>
+            <div className='countryDetails'>
+              <p>Carnival Infopark, Phase 1,
+
+                First Floor Infopark,
+
+                Kakkanad, Kochi, Kerala 682042
+
+                Contact +91 9048323666</p>
+            </div>
+          </div >
+        </div>
+      </div >
+
+
+    </div>
+
   )
 }

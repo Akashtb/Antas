9ace2152ba67e2f14933ef09c1873e3ae09b959a
akashtb <akashtb21@gamil.com>
2023-03-16 09:18:11 +0530

    message

--- antas-client/src/components/footer/Footer.jsx
+++ antas-client/src/components/footer/Footer.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='py-5 text-center'>
+        &copy; 2019 &nbsp; Antas
+    </div>
   )
 }
